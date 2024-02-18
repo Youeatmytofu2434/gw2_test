bcf6e9cae73b108a9f6beffb2d3ec89c691a994e
Man Luu <!Nandemonaiya123>
2024-02-17 21:15:58 -0600

    Added the ability to auto control

--- gw2_auto_fishing.py
+++ gw2_auto_fishing.py
@@ -1,29 +1,82 @@
 import numpy as np
 import cv2
-from mss import mss
-from PIL import Image
 import pyautogui
+import pydirectinput
+from mss import mss
+from numpy import ndarray
 
-def get_screen_size() -> dict:
+
+YELLOW_BOX_THRESHOLD = 0.95
+
+
+def get_screen_to_capture() -> dict:
     screenWidth, screenHeight = pyautogui.size()
 
     screenToCapture = {
         'top' : int( 1/3 * screenHeight ),
         'left' : int( 1/3 * screenWidth ),
         'width' : 1100,
-        'height' : 650
+        'height' : 750
     }
 
     return screenToCapture
 
+def get_object_dimensions( srcImg: ndarray ):
+    srcImgWidth = srcImg.shape[1]
+    srcImgHeight = srcImg.shape[0]
+    return ( srcImgWidth, srcImgHeight )
+
+def group_rectangles( potentialMatches: ndarray, objectImg: ndarray ):
+    ( objectWidth, objectHeight ) = get_object_dimensions(
+        objectImg
+    ) 
+
+    detectedObjects = [] # Holds all potential rectangles to
+                                # group them later on.
+
+    # Grab everything that the program think is the yellow box.
+    yLoc, xLoc = np.where( potentialMatches >= YELLOW_BOX_THRESHOLD )
+
+    # Make sure there are at least 2 rectangles for each instance 
+    # for grouping to work.
+    for( x, y ) in zip( xLoc, yLoc ):
+        detectedObjects.append(
+            [x, y, objectWidth+x, objectHeight+y]
+        )
+        detectedObjects.append( 
+            [x, y, objectWidth+x, objectHeight+y] 
+        )
+
+    detectedObjects, _ = cv2.groupRectangles(
+        detectedObjects,
+        groupThreshold = 1, 
+        eps = 0.2
+    )
+
+    return detectedObjects
+
+def is_box_on_left( greenBoxRec, yellowBoxRec ):
+    if( ( greenBoxRec[0] - yellowBoxRec[0] ) < 0 ):
+        return True
+    return False
+
+def move_yellow_box( greenBoxRecs, yellowBoxRecs ):
+    if( len( greenBoxRecs ) > 0 and
+        len( yellowBoxRecs) > 0 ):
+        # Check if greenbox is on the left.
+        if( is_box_on_left(
+            greenBoxRecs[0], yellowBoxRecs[0]
+        ) ):
+            pydirectinput.keyUp( 'd' )
+            pydirectinput.keyDown( 'a' )
+        else:
+            pydirectinput.keyUp( 'a' )
+            pydirectinput.keyDown( 'd' )
 
 if __name__ == "__main__":
 
-    # capture current screen.
-    # find greenBox and yellowBox.
-
     greenBox = cv2.imread( 'images/greenBox.png' )
-    yellowBox = cv2.imread( 'images/yellowBox.png' )
+    yellowBox = cv2.imread( 'images/yellowBoxTest.png' )
 
     greenBox = cv2.cvtColor( greenBox, cv2.COLOR_RGB2GRAY )
     yellowBox = cv2.cvtColor( yellowBox, cv2.COLOR_RGB2GRAY )
@@ -31,12 +84,14 @@ if __name__ == "__main__":
     with mss() as sct:
         while True:
             # Screenshot predefined region.
-            screenShot = sct.grab( get_screen_size() )
+            screenShot = sct.grab( get_screen_to_capture() )
             screenShot = np.array( screenShot )
 
             # Convert 2 grayscale to sastify the dimension requirement.
-            grayScaleScreenShot = cv2.cvtColor( screenShot,
-                                               cv2.COLOR_RGBA2GRAY )
+            grayScaleScreenShot = cv2.cvtColor(
+                screenShot,
+                cv2.COLOR_RGBA2GRAY
+            )
             
             # Template matching, grab anything that resembles our objects.
             greenMatches = cv2.matchTemplate(
@@ -49,35 +104,33 @@ if __name__ == "__main__":
                 yellowBox,
                 cv2.TM_CCOEFF_NORMED
             )
+
+
+            potentialGreenBoxes  = group_rectangles(
+                greenMatches,
+                greenBox
+            )
+            potentialYellowBoxes = group_rectangles(
+                yellowMatches,
+                yellowBox
+            )
+
+            move_yellow_box( potentialGreenBoxes, potentialYellowBoxes )
+
+            # DRAW on screen.
+            for ( x1,y1,x2,y2 ) in potentialGreenBoxes:
+                cv2.rectangle( screenShot, ( x1,y1 ),
+                            ( x2,y2 ), (0,255,255),
+                            2
+                )
             
-            greenMinVal, greenMaxVal, greenMinLoc, greenMaxLoc = cv2.minMaxLoc( greenMatches )
-            yellowMinVal, yellowMaxVal, yellowMinLoc, yellowMaxLoc = cv2.minMaxLoc( yellowMatches )
-
-            # greenBox & yellowBox' shapes.
-            greenBoxWidth   = greenBox.shape[1]
-            greenBoxHeight  = greenBox.shape[0]
-            yellowBoxWidth  = yellowBox.shape[1]
-            yellowBoxHeight = yellowBox.shape[0]
-
-            greenBoxLoc2 = ( 
-                greenMaxLoc[0] + greenBoxWidth,
-                greenMaxLoc[1] + greenBoxHeight
-            )
-            yellowBoxLoc2 = (
-                yellowMaxLoc[0] + yellowBoxWidth,
-                yellowMaxLoc[1] + yellowBoxHeight
-            )
-
-            # Draw on to our screen.
-            cv2.rectangle( screenShot, greenMaxLoc,
-                           greenBoxLoc2,
-                           (0,255,255), 2 )
-            cv2.rectangle( screenShot, yellowMaxLoc,
-                           yellowBoxLoc2,
-                           (255,0,255), 2 )
-
+            for ( x1,y1,x2,y2 ) in potentialYellowBoxes:
+                cv2.rectangle( screenShot, ( x1,y1 ),
+                            ( x2,y2 ), (255,0,255),
+                            2
+                )
+            
             cv2.imshow( 'Screen', screenShot )
-
 
             # Quit by pressing q.
             if( cv2.waitKey(1) & 0xFF ) == ord( 'q' ):
